71ae963822b686edc4edc7d225f2ba6310e7f3d4
lilao8 <liamgmai1@gmail.com>
2021-07-26 12:51:21 -0400

    add paragraph

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Netlify Testing</h1>
+      <h1>Minimal Viable Mondays</h1>
+      <p>this is a paragraph</p>
     </div>
   );
 }
